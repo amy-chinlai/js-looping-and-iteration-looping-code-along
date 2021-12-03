e9e903adf79254cae6316f01b8ca6f5e3303add7
amy-chinlai <amy@actionnetwork.org>
2021-12-03 09:52:12 -0500

    commit tree 913348580cf0579b24e7fb4b7d3d936aba5fa940

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// const names = ["Lisa", "Kaitlin", "Jan"]
+// const gift_type = "surprise"
+
+function writeCards(names, gift_type) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${gift_type} gift!`);
+    }
+    return cards
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer)
+        integer = integer - 1
+    }
+}
